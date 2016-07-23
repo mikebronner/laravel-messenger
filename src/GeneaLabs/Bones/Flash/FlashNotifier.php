dc0ac6d2c17f7aab976a503bc560596916ead625
Mike Bronner <mike@genealabs.com>
2016-07-23 15:53:51 -0700

    Add auto-hide functionality

--- src/GeneaLabs/Bones/Flash/FlashNotifier.php
+++ src/GeneaLabs/Bones/Flash/FlashNotifier.php
@@ -4,35 +4,36 @@ use Illuminate\Support\Facades\Session;
 
 final class FlashNotifier
 {
-    public function success($message)
+    public function success($message, $autoHide = true)
     {
-        $this->message($message, 'success');
+        $this->message($message, 'success', $autoHode);
     }
 
-    public function info($message)
+    public function info($message, $autoHide = false)
     {
-        $this->message($message);
+        $this->message($message, 'info', $autoHode);
     }
 
-    public function warning($message)
+    public function warning($message, $autoHide = false)
     {
-        $this->message($message, 'warning');
+        $this->message($message, 'warning', $autoHode);
     }
 
-    public function danger($message)
+    public function danger($message, $autoHide = false)
     {
-        $this->message($message, 'danger');
+        $this->message($message, 'danger', $autoHode);
     }
 
     public function modal($message)
     {
-        $this->message($message);
+        $this->message($message, null, false);
         Session::put('flashNotification.modal', true);
     }
 
-    private function message($message, $level = 'info')
+    private function message($message, $level = 'info', $autoHide = false)
     {
         Session::put('flashNotification.message', $message);
         Session::put('flashNotification.level', $level);
+        Session::put('flashNotification.autoHide', $autoHide);
     }
 }
