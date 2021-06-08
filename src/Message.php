3b207661d429c339ffa2e650c8b9a5001aaf7a2b
Mike Bronner <mike@genealabs.com>
2021-06-08 15:18:44 -0700

    Added unique id for each message.

--- src/Message.php
+++ src/Message.php
@@ -1,10 +1,16 @@
-<?php namespace GeneaLabs\LaravelMessenger;
+<?php
 
+declare(strict_types=1);
+
+namespace GeneaLabs\LaravelMessenger;
+
+use Illuminate\Support\Str;
 use Jenssegers\Model\Model;
 
 class Message extends Model
 {
     protected $appends = [
+        "id",
         'autoHide',
         'framework',
         'message',
@@ -16,6 +22,7 @@ class Message extends Model
         'autoHide' => 'boolean',
     ];
     protected $fillable = [
+        "id",
         'autoHide',
         'framework',
         'message',
@@ -24,33 +31,41 @@ class Message extends Model
         'type',
     ];
 
-    public function getAutoHideAttribute() : bool
+    public function getIdAttribute(): string
+    {
+         $this->attributes["id"] = $this->attributes["id"]
+             ?? Str::random(16);
+
+        return $this->attributes["id"];
+    }
+
+    public function getAutoHideAttribute(): bool
     {
         return ($this->attributes['autoHide'] === true);
     }
 
-    public function getFrameworkAttribute() : string
+    public function getFrameworkAttribute(): string
     {
         return $this->attributes['framework']
             ?: config('genealabs-laravel-messenger.framework');
     }
 
-    public function getMessageAttribute() : string
+    public function getMessageAttribute(): string
     {
         return $this->attributes['message'] ?: '';
     }
 
-    public function getLevelAttribute() : string
+    public function getLevelAttribute(): string
     {
         return $this->attributes['level'] ?: 'info';
     }
 
-    public function getTitleAttribute() : string
+    public function getTitleAttribute(): string
     {
         return $this->attributes['title'] ?: '';
     }
 
-    public function getTypeAttribute() : string
+    public function getTypeAttribute(): string
     {
         return $this->attributes['type'] ?: 'alert';
     }
